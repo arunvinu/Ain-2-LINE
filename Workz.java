5789ef158489ddca255e6195494b7aa330d0cb43
arunvinu <arunkumarxworkz@gmail.com>
2021-12-27 13:03:46 +0545

    2 in one line

---
+++ Workz.java
@@ -0,0 +1,16 @@
+class Workz{
+public static void mai1n(String [ ]args){
+System.out.println("Squad for team of cricket tournment");
+
+	for(int player=0; player<5; player++){
+	System.out.print(Squad[player]+"\t");
+	}
+	System.out.println();
+
+for(int player=5; player<Squad.length;player++){
+System.out.print(Squad[player]+"\t");
+}
+System.out.println();
+System.out.println("Stand by players wiil be announced shortly");
+}
+}
